e8142ce7fccd176b2f5dd2d2c9c3df3fc16f68cb
Diogo Lima <diogoaraujolima@gmail.com>
2025-04-17 15:41:13 +0200

    updated image_to_pose

--- src/image_to_pose/include/image_to_pose.hpp
+++ src/image_to_pose/include/image_to_pose.hpp
@@ -15,22 +15,35 @@ public:
 
 private:
   void image_callback(sensor_msgs::msg::Image::UniquePtr msg);
-  cv::Point3f estimate_position(const cv::Point2f& image_point, float radius_pixels);
+  
+  cv::Point2f estimate_position(const cv::Point2f &image_point, const float &radius_pixels);
 
   rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
+
   size_t count_;
   
-  
-  // Camera intrinsic parameters (replace with real calibration if needed)
-  float fx_ = 525.0;
-  float fy_ = 525.0;
-  float cx_ = 320.0;
-  float cy_ = 240.0;
-  float real_ball_radius_m_ = 0.05; // 5cm radius
+  bool imageProcessing(sensor_msgs::msg::Image::UniquePtr &msg, cv::Mat &image);
+  bool detect_ball(const cv::Mat &mask, cv::Point2f &center, float &radius);
+  void displayDetection(cv::Mat &image, const cv::Point2f &center, const float &radius, const geometry_msgs::msg::Pose &pose);
 
+
+  // Camera intrinsic parameters from camera matrix
+  // fx is the focal length in pixels
+  // cx is the optical center in pixels
+  // These values are obtained from OpenCV camera calibration
+  float cx_ = 320.0;
+  float fx_ = 525.0;
+
+  // Real ball radius in meters
+  float real_ball_radius_m_ = 0.0615; 
+
+  // Name of windoe with ball detected
   const std::string window_name_ = "Green Ball Detection";
 
+  //
   rclcpp::Publisher<geometry_msgs::msg::Pose>::SharedPtr pose_publisher_;
-  void calculate_pose(cv::Point3f position);
+
+  // Calculates the
+  geometry_msgs::msg::Pose calculate_pose(cv::Point2f &position2D);
   
 };
--- src/image_to_pose/src/image_to_pose.cpp
+++ src/image_to_pose/src/image_to_pose.cpp
@@ -12,147 +12,251 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/* This example creates a subclass of Node and uses a fancy C++11 lambda
- * function to shorten the callback syntax, at the expense of making the
- * code somewhat more difficult to understand at first glance. */
+//===============================================================================================
+// Filename : image_to_pose.cpp.cpp
+// Authors : Diogo Lima, Ruben Koudijs
+// Group : SDfR-CBL 7
+// License : Apache 2.0
+// Description : This code is a ROS2 node that subscribes to an image topic, processes the image
+// to detect a green ball, and estimates its 3D position. The estimated position is then
+// published as a Pose message (x - Length, Theta - Orientation).
+//===============================================================================================
 
 #include "image_to_pose.hpp"
 #include <iomanip>
 #include <sstream>
 #include <cmath>
 
-
-
+// Input: Nothing
+// Output: Nothing
+// Decription: Constructor of ImageToPose. It initializes a node ("image_to_pose"), creates a subscription
+// to the "/image" topic, and creates a publisher for the "ball_pose" topic. It also initializes the count variable.
 ImageToPose::ImageToPose() : Node("image_to_pose"), count_(0)
 {
+  // Creates a subscription to the "/image" topic
+  // Allows for 10 messages in the queue
+  // A callback function is called when a new message is received to process the image
   subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
       "/image", 10,
       std::bind(&ImageToPose::image_callback, this, std::placeholders::_1));
 
-      pose_publisher_ = this->create_publisher<geometry_msgs::msg::Pose>("/ball_pose", 10);
+  // Creates a publisher for the "/ball_pose" topic
+  // Allows for 10 messages in the queue
+  // The publisher sends messages of type geometry_msgs::msg::Pose
+  pose_publisher_ = this->create_publisher<geometry_msgs::msg::Pose>("/ball_pose", 10);
 }
 
+// Input: msg - Image message
+// Output: Nothing
+// Description: This function is called when a new image message is received.
+// It processes the image to detect a green ball, estimates its 3D position, and publishes the pose (x - Length, Theta - Orientation).
+// It uses OpenCV to convert the image to HSV color space, creates a mask for the green color,
+// detects contours, and finds the largest contour. It then estimates the position of the ball in 3D space
+// calculates and publishes the pose as a geometry_msgs::msg::Pose message.
+// It also draws the detected ball on the image and displays it using OpenCV.
+// If no ball is detected, it publishes a null pose and displays a warning message.
+// Note: The function assumes that the camera is calibrated and uses the camera intrinsic parameters (camera matrix)
+// (fx, fy, cx, cy) to convert the 2D image point to a 3D point in the camera coordinate system.
 void ImageToPose::image_callback(sensor_msgs::msg::Image::UniquePtr msg)
 {
+  // Logs having received a new image
   RCLCPP_INFO(this->get_logger(), "Received image #%zu", count_++);
 
-  // Convert to OpenCV
-  cv_bridge::CvImagePtr cv_ptr;
-  try
-  {
-    cv_ptr = cv_bridge::toCvCopy(*msg, "bgr8");
-  }
-  catch (cv_bridge::Exception &e)
-  {
-    RCLCPP_ERROR(this->get_logger(), "cv_bridge exception: %s", e.what());
-    return;
-  }
+  // Processes the image
+  cv::Mat image;
+  bool checkImageProcessing = imageProcessing(msg, image);
+  if (!checkImageProcessing){
+    // detects an error in the image processing
+    RCLCPP_WARN(this->get_logger(), "Image Processing Error:");
 
-  // Converts the image to HSV color space
-  // and creates a mask for the green color
-  cv::Mat hsv, mask;
-  cv::cvtColor(cv_ptr->image, hsv, cv::COLOR_BGR2HSV);
-  cv::inRange(hsv, cv::Scalar(35, 100, 100), cv::Scalar(85, 255, 255), mask);
-
-  // Countours detection
-  std::vector<std::vector<cv::Point>> contours;
-  cv::findContours(mask, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-
-  // Checks for the green ball
-  if (contours.empty())
-  {
-    geometry_msgs::msg::Pose nullPose;  
-
+    // send null velocity
+    geometry_msgs::msg::Pose nullPose;
     nullPose.position.x = 0.0;
     nullPose.orientation.z = 0.0;
 
     // Publish the message
     pose_publisher_->publish(nullPose);
-
-    RCLCPP_WARN(this->get_logger(), "No green ball found.");
-    cv::imshow(window_name_, cv_ptr->image);
-    cv::waitKey(1);
-    return;
+    return; 
   }
 
-
-  // Find the largest contour
-  auto max_contour = std::max_element(contours.begin(), contours.end(),
-                                      [](const auto &a, const auto &b)
-                                      {
-                                        return cv::contourArea(a) < cv::contourArea(b);
-                                      });
-
-  // tries to find the ball (center, radius) in the mask
   cv::Point2f center;
   float radius;
-  cv::minEnclosingCircle(*max_contour, center, radius);
 
-  // Check if the detected radius is valid
-  if (radius < 5.0)
+  bool checkBallDetection = detect_ball(image, center, radius);
+
+  if (!checkBallDetection)
   {
-    RCLCPP_WARN(this->get_logger(), "Detected ball is too small.");
-    cv::imshow(window_name_, cv_ptr->image);
+    RCLCPP_WARN(this->get_logger(), "No valid green ball found.");
+    
+    // to change======================================
+    geometry_msgs::msg::Pose nullPose;
+    nullPose.position.x = 0.0;
+    nullPose.orientation.z = 0.0;
+
+    // Publish the message
+    pose_publisher_->publish(nullPose);
+    // ===============================================
+
+    cv::imshow(window_name_, image);
     cv::waitKey(1);
     return;
   }
 
-  // Estimate the position of the ball in 3D space
-  cv::Point3f position = estimate_position(center, radius);
-  RCLCPP_INFO(this->get_logger(), "Ball position (x=%.2f, y=%.2f, z=%.2f) m", position.x, position.y, position.z);
+  // Estimate the position of the ball in 2D space (Oxz plane horizontal plane of the camera)
+  cv::Point2f position2D = estimate_position(center, radius);
 
-  // Draw detection on image
-  cv::circle(cv_ptr->image, center, static_cast<int>(radius), cv::Scalar(0, 0, 255), 2);
+  // The 2D position of the ball is loged for debugging
+  RCLCPP_INFO(this->get_logger(), "Ball position (x=%.2f, z=%.2f) m", position2D.x, position2D.y);
+
+  // calculates and publishes the pose
+  geometry_msgs::msg::Pose pose = calculate_pose(position2D);
+
+  // Display the detected ball on the image
+  displayDetection(image, center, radius, pose);
+}
+
+bool ImageToPose::imageProcessing(sensor_msgs::msg::Image::UniquePtr &msg, cv::Mat &image)
+{
+  // Convert from ROS2 image message to OpenCV image
+  // Initializes a pointer for the image
+  
+
+  // Uses a try cacth for attempting to convert the image
+  // If it fails, it logs an error message and returns
+  // The image is converted to BGR8 format
+  cv_bridge::CvImagePtr cv_ptr;
+  try
+  {
+    cv_ptr = cv_bridge::toCvCopy(*msg, "bgr8");
+  }
+  catch (const std::exception &e)
+  {
+    RCLCPP_WARN(this->get_logger(), ("image Processing Error: " + std::string(e.what())).c_str());
+    
+    // Return with a false if the image conversion fails
+    return false; 
+  }
+
+  // Convert the image to HSV color space
+  cv::Mat hsv, mask;
+  cv::cvtColor(cv_ptr->image, hsv, cv::COLOR_BGR2HSV);
+
+  // Create a mask for the green color and saves it in the image input argument
+  cv::inRange(hsv, cv::Scalar(35, 100, 100), cv::Scalar(85, 255, 255), mask);
+
+  // Save the processed image in image 
+  image = mask;
+
+  // Confirm that the Image Processing was successful
+  return true;
+}
+
+
+bool ImageToPose::detect_ball(const cv::Mat &mask, cv::Point2f &center, float &radius)
+{
+  std::vector<std::vector<cv::Point>> contours;
+  cv::findContours(mask, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+  if (contours.empty()) return false;
+
+  auto max_contour = *std::max_element(contours.begin(), contours.end(),
+                                       [](const auto &a, const auto &b) {
+                                         return cv::contourArea(a) < cv::contourArea(b);
+                                       });
+
+  cv::minEnclosingCircle(max_contour, center, radius);
+  return radius >= 5.0;
+}
+
+
+
+
+// Input: image_point - 2D point in the image (in pixels)
+//        radius_pixels - radius of the ball (in pixels)
+// Output: 2D point (x,z) in meters
+// Description: Estimates the 2D position (Oxz plane horizontal plane of the camera) of the ball in meters.
+// This function assumes a pinhole camera model. The function uses the camera intrinsic parameters
+// (fx, cx) to convert the 2D image point to a 3D point in the camera coordinate system.
+cv::Point2f ImageToPose::estimate_position(const cv::Point2f &image_point, const float &radius_pixels)
+{
+  // z is the depth position of the ball to the camera
+  float z = (real_ball_radius_m_ * fx_) / radius_pixels;
+
+  // x is the transverse position of the ball to the camera
+  float x = (image_point.x - cx_) * z / fx_;
+
+  // Note: y (vertical position) is not computed here to save computation,
+  // as it is currently unused in downstream pose calculations.
+
+  // Return the estimated position as a 3D point in meters
+  return cv::Point2f(x, z);
+}
+
+// Input: position - 2D position of the ball in meters
+// Output: pose - x - Length [m], Theta - Orientation [rad]
+// Description: Calculates the pose of the ball base on the real 2D position of the ball
+// and publishes it to the "/ball_pose" topic.
+geometry_msgs::msg::Pose ImageToPose::calculate_pose(cv::Point2f &position2D)
+{
+  // Create a new Pose variable
+  geometry_msgs::msg::Pose pose;
+
+  // Calculate the x "pose" position of the ball
+  // The x "pose" position is the distance from the camera to the ball
+  // we don't use the y posittion of the ball, we assume that the ball is projected into the xOz plane
+  // The x "pose" is in meters
+  pose.position.x = sqrt(pow(position2D.x, 2) + pow(position2D.y, 2));
+
+  // Calculate Theta "pose" orientation of the ball in a 2D plane
+  // and store it in the z orientation of the Pose message
+  // The theta "pose" orientation is the angle between the front acis of the camera
+  // and the line connecting the camera to the ball
+  // The angle is in radians
+  pose.orientation.z = (M_PI / 2) - atan2(position2D.y, position2D.x);
+
+  // Publishes the Pose message
+  pose_publisher_->publish(pose);
+
+  return pose;
+}
+
+// Input:, image - OpenCV image, center of the ball, radius of the ball, pose of the ball
+// Output: Nothing
+// Description: Displays the detected ball on the image.
+// This function draws a circle around the detected ball using OpenCV.
+// Also display the pose of the ball in the image.
+void ImageToPose::displayDetection(cv::Mat &image,
+                                   const cv::Point2f &center,
+                                   const float &radius,
+                                   const geometry_msgs::msg::Pose &pose)
+{
+  // Draw a circle detection on image
+  cv::circle(image, center, static_cast<int>(radius), cv::Scalar(0, 0, 255), 2);
+
+  // Label for displaying the pose values
   std::ostringstream label;
-  label << "[" << std::fixed << std::setprecision(2)
-        << position.x << ", " << position.y << ", " << position.z << "]";
+  label << "(" << std::fixed << std::setprecision(2)
+                             << pose.position.x << ", " << pose.orientation.z << ") ";
 
-  // Draw a red dot at the center of the ball
-  cv::circle(cv_ptr->image, center, 3, cv::Scalar(0, 0, 255), -1);
-
-  cv::putText(cv_ptr->image, label.str(), center + cv::Point2f(-90, -(radius + 10)),
+  // Draw the label on the image
+  cv::putText(image, label.str(), center + cv::Point2f(-90, -(radius + 10)),
               cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 255), 2);
 
-  cv::imshow(window_name_, cv_ptr->image);
+  // Draw a red dot at the center of the ball
+  cv::circle(image, center, 3, cv::Scalar(0, 0, 255), -1);
+
+  // Display the image with the detected ball
+  cv::imshow(window_name_, image);
+
+  // Display the image for 1 ms
   cv::waitKey(1);
-
-  // calculates and publishes the position in 2D
-  calculate_pose(position);
 }
 
-// Description: Estimates the 3D position of the ball in meters
-// Input: image_point - 2D point in the image (in pixels)
-//        radius_pixels - radius of the ball in pixels
-// Output: 3D point in meters
-// Note: This function assumes a pinhole camera model and that the ball is spherical
-//       and the camera is calibrated. The function uses the camera intrinsic parameters
-//       (fx, fy, cx, cy) to convert the 2D image point to a 3D point in the camera coordinate system.
-cv::Point3f ImageToPose::estimate_position(const cv::Point2f &image_point, float radius_pixels)
-{
-  float z = (real_ball_radius_m_ * fx_) / radius_pixels;
-  float x = (image_point.x - cx_) * z / fx_;
-  float y = (image_point.y - cy_) * z / fy_;
-  return cv::Point3f(x, y, z);
-}
-
-void ImageToPose::calculate_pose(cv::Point3f position){
-  // Create a Pose message
-  geometry_msgs::msg::Pose pose_msg;
-
-  // Set the x position of the ball
-  pose_msg.position.x = sqrt(pow(position.x,2) + pow(position.z, 2));
-
-  // Set z orientation of the ball in a 2D plane
-  pose_msg.orientation.z = (M_PI/2) - atan2(position.z, position.x);
-
-  // Log and publish the pose
-  //RCLCPP_INFO(this->get_logger(), "Publishing Pose: x=%.2f, y=%.2f, z=%.2f, theta=%.2f", 
-   //           pose_msg.position.x, pose_msg.orientation.z);
-  
-  // Publish the message
-  pose_publisher_->publish(pose_msg);
-}
-
+// Input: argc - number of command line arguments
+//        argv - array of command line arguments
+// Output: 0 on success, non-zero on failure
+// Description: Initializes the ROS2 node, creates an instance of ImageToPose,
+//              and starts the ROS2 event loop to process incoming messages.
 int main(int argc, char *argv[])
 {
   rclcpp::init(argc, argv);
@@ -160,4 +264,3 @@ int main(int argc, char *argv[])
   rclcpp::shutdown();
   return 0;
 }
-
